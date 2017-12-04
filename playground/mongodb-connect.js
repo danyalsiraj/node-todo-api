8eb47efcb0881f5d23dd8933630fdaf9fe47bd5d
danyalsiraj <danyal61@gmail.com>
2017-12-04 01:30:36 -0500

    inserting in mongo DB

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,5 +7,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApi', (err, db) => { //connec
     return console.log('Unable to connect to database');
   }
   console.log('connected to mongodb server');
+  ////To add to collection
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to Do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('unable to insert in the DB', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2)); //ops stores all the documents that were inserted
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'Danyal',
+    age: 23,
+    location: 'Canada'
+  }, (err, result) => {
+    if (err) {
+      return console.log('unable to insert user');
+    }
+    console.log(result.ops);
+  });
   db.close();
 });
